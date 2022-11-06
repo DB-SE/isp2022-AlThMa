ccbc54c66a6c7d04d2ba4104cda3af249eb488b0
thomasbubi <4480324+thomasbubi@users.noreply.github.com>
2022-11-06 23:30:26 +0100

    add another Edge constructors

--- src/Edge.java
+++ src/Edge.java
@@ -1,7 +1,20 @@
 public class Edge {
-    int node1, node2, value, direction;
-    public static final int NODE_1 = 1;
-    public static final int NODE_2 = 2;
+    int node1, node2, value;
+    boolean direction;
+    public static final boolean NODE_1 = false;
+    public static final boolean NODE_2 = true;
+
+    /**
+     *
+     * @param node1 Index of one Node
+     * @param node2 Index of another Node
+     */
+    public Edge(int node1, int node2) {
+        this.node1 = node1;
+        this.node2 = node2;
+        this.value = 0;
+        this.direction = NODE_2;
+    }
 
     /**
      * Constructor for an undirected Edge
@@ -20,15 +33,30 @@ public class Edge {
      *
      * @param node1 Index of one Node
      * @param node2 Index of another Node
-     * @param direction Index of the node to which the edge is directed | can be 1 or 2, or 0 for no direction
-     * @param value Labeldirection of the node
+     * @param direction Boolean value whether the direction should be towards second node
      */
-    public Edge(int node1, int node2, int direction, int value) {
+    public Edge(int node1, int node2, boolean direction) {
+
+        this.node1 = node1;
+        this.node2 = node2;
+        this.value = 0;
+        this.direction = direction;
+
+    }
+
+    /**
+     *
+     * @param node1 Index of one Node
+     * @param node2 Index of another Node
+     * @param direction Boolean value whether the direction should be towards second node
+     * @param value Label of the node
+     */
+    public Edge(int node1, int node2, boolean direction, int value) {
 
         this.node1 = node1;
         this.node2 = node2;
         this.value = value;
-        this.direction = direction < 1 ? NODE_1 : direction > 2 ? NODE_2 : direction;
+        this.direction = direction;
 
     }
 }
