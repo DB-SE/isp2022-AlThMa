3a222623272ce08aec443fa9bff88fbeea8c0c00
thomasbubi <4480324+thomasbubi@users.noreply.github.com>
2022-11-06 22:00:11 +0100

    add second Edge Constructor

--- src/Edge.java
+++ src/Edge.java
@@ -1,9 +1,34 @@
 public class Edge {
-    int node1, node2, value;
+    int node1, node2, value, direction;
+    public static final int NODE_1 = 1;
+    public static final int NODE_2 = 2;
 
+    /**
+     * Constructor for an undirected Edge
+     * @param node1 Index of one Node
+     * @param node2 Index of another Node
+     * @param value Label of the node
+     */
     public Edge(int node1, int node2, int value) {
         this.node1 = node1;
         this.node2 = node2;
         this.value = value;
+        this.direction = NODE_2;
+    }
+
+    /**
+     *
+     * @param node1 Index of one Node
+     * @param node2 Index of another Node
+     * @param direction Index of the node to which the edge is directed | can be 1 or 2, or 0 for no direction
+     * @param value Labeldirection of the node
+     */
+    public Edge(int node1, int node2, int direction, int value) {
+
+        this.node1 = node1;
+        this.node2 = node2;
+        this.value = value;
+        this.direction = direction < 1 ? NODE_1 : direction > 2 ? NODE_2 : direction;
+
     }
 }
